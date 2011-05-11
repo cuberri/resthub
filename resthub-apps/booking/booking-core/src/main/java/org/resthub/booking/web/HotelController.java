af3fdb30072eb58153f6d0a9b78608f703958330
Baptiste Meurant <baptiste.meurant@gmail.com>
2011-05-11 18:08:30 +0200

    remove unused import

--- resthub-apps/booking/booking-core/src/main/java/org/resthub/booking/web/HotelController.java
+++ resthub-apps/booking/booking-core/src/main/java/org/resthub/booking/web/HotelController.java
@@ -15,6 +15,7 @@ import org.resthub.booking.model.Hotel;
 import org.resthub.booking.service.HotelService;
 import org.resthub.web.controller.GenericResourceController;
 import org.resthub.web.response.PageResponse;
+import org.synyx.hades.domain.Page;
 import org.synyx.hades.domain.PageRequest;
 
 /**
@@ -22,38 +23,38 @@ import org.synyx.hades.domain.PageRequest;
  */
 @Path("/hotel")
 @Named("hotelController")
-public class HotelController extends GenericResourceController<Hotel, HotelService> {
+public class HotelController extends
+        GenericResourceController<Hotel, HotelService> {
 
-	/**
-	 * {@InheritDoc}
-	 */
-	@Inject
-	@Named("hotelService")
-	@Override
-	public void setService(HotelService service) {
-		this.service = service;
-	}
+    /**
+     * {@InheritDoc}
+     */
+    @Inject
+    @Named("hotelService")
+    @Override
+    public void setService(HotelService service) {
+        this.service = service;
+    }
 
-	/**
-	 * @return all hotels containing the value given in parameter
-	 * If query string is empty, fetch all hotels in DB
-	 */
-	@GET
-	@Path("/search")
-	@Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-	public Response searchHotels(
-			@QueryParam("q") String query,
-			@QueryParam("page") @DefaultValue("0") Integer page,
-			@QueryParam("size") @DefaultValue("5") Integer size) {
-		
-		PageResponse<Hotel> hotels;
+    /**
+     * @return all hotels containing the value given in parameter If query
+     *         string is empty, fetch all hotels in DB
+     */
+    @GET
+    @Path("/search")
+    @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+    public Response searchHotels(@QueryParam("q") String query,
+            @QueryParam("page") @DefaultValue("0") Integer page,
+            @QueryParam("size") @DefaultValue("5") Integer size) {
 
-		hotels = new PageResponse<Hotel>(
-                    this.service.find(query, new PageRequest(page, size)));
+        Page<Hotel> hotels;
 
-		if (hotels == null) {
-			return Response.status(Status.BAD_REQUEST).entity("Bad query.").build();
-		}
-		return Response.ok(hotels).build();
-	}
+        hotels = this.service.find(query, new PageRequest(page, size));
+
+        if (hotels == null) {
+            return Response.status(Status.BAD_REQUEST).entity("Bad query.")
+                    .build();
+        }
+        return Response.ok(new PageResponse<Hotel>(hotels)).build();
+    }
 }
